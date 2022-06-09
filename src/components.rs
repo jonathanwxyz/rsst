901278130f907e3bdad823f1a6e1dab8be06559a
Jonathan <mail4jonathanw@gmail.com>
2022-06-09 17:24:36 +0100

    now displays the html in feed descriptions

--- src/components.rs
+++ src/components.rs
@@ -1,2 +1,3 @@
 pub mod article_card;
 pub mod feed_card;
+pub mod safe_html;
--- src/components/article_card.rs
+++ src/components/article_card.rs
@@ -1,4 +1,5 @@
 use crate::lib::Article;
+use crate::components::safe_html::SafeHtml;
 
 use yew::prelude::*;
 
@@ -9,10 +10,23 @@ pub struct ArticleCardProps {
 
 #[function_component(ArticleCard)]
 pub fn article_card(ArticleCardProps { article }: &ArticleCardProps) -> Html {
+    
+    let desc = &article.description;
+    let displayed_desc: Html;
+    if desc.starts_with('<') {
+        displayed_desc = html! {
+            <SafeHtml html={desc.clone()}/>
+        }
+    } else {
+        displayed_desc = html! {
+            <p>{desc}</p>
+        }
+    }
+
     html! {
         <div>
             <h3>{&article.title}</h3>
-            <p>{&article.description}</p>
+            {displayed_desc}
         </div>
     }
 }
